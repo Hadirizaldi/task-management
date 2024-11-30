e102a2886bbabdec354517b0b9b9e96289f3010a
Megatorn <hadirizaldi.hr@gmail.com>
2024-12-01 00:11:40 +0700

    deleted console log

--- public/assets/js/signOut.js
+++ public/assets/js/signOut.js
@@ -1,12 +1,10 @@
 import { User } from './User.js';
 
 $(function() {
-  console.log("================== sign out =================");
-  
   $("#btn-logout").click(function(e) {
     e.preventDefault();
     User.signOut();
-    
+
     window.location.href = "/public/signin.html";
   })
 })
